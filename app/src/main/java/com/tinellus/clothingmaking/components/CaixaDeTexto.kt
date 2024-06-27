fdd58e7d43d334cc6787ec1ca6246b996d54738e
gil-fullstack <gilservicosweb@gmail.com>
2024-06-27 09:34:43 -0300

    update with created a default textField with outlinedTextField

---
+++ app/src/main/java/com/tinellus/clothingmaking/components/CaixaDeTexto.kt
@@ -0,0 +1,57 @@
+package com.tinellus.clothingmaking.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CaixaDeTexto(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier,
+    label: String,
+    maxLines: Int
+){
+
+   OutlinedTextField(
+       value = value,
+       onValueChange,
+       modifier,
+       label = {
+           Text(text = label)
+       },
+       maxLines = maxLines,
+       textStyle = TextStyle(color = Color.Gray, fontSize = 20.sp),
+
+       colors = TextFieldDefaults.textFieldColors(
+           disabledTextColor = Color.Gray,
+           focusedIndicatorColor = Color.Blue,
+           unfocusedIndicatorColor = Color.Gray,
+           disabledIndicatorColor = Color.LightGray,
+           cursorColor = Color.Green,
+           errorCursorColor = Color.Magenta,
+       ),
+       shape = RoundedCornerShape(20.dp)
+   )
+}
+
+@Composable
+@Preview
+private fun CaixaDeTextoPreview(){
+    CaixaDeTexto(
+        value = "Gilvan",
+        onValueChange = {},
+        modifier = Modifier.background(Color.White).fillMaxWidth(),
+        label = "Descrição",
+        maxLines = 1
+        )
+}
--- app/src/main/java/com/tinellus/clothingmaking/view/SalvarTarefas.kt
+++ app/src/main/java/com/tinellus/clothingmaking/view/SalvarTarefas.kt
@@ -1,35 +1,52 @@
 package com.tinellus.clothingmaking.view
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.tinellus.clothingmaking.R
 
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SalvarTarefas(
     navController: NavController
 ){
-    Column(modifier = Modifier.fillMaxSize().background(Color.Cyan),
-        verticalArrangement = Arrangement.Center,
-
-        ) {
-
-        Button(
-            onClick = { navController.navigate("myMainView") },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-
-        ) {
-            Text(text = "Go Back main", fontWeight = FontWeight.Bold)
-        }
+    Scaffold {
+        TopAppBar(title = {
+                Text(text = stringResource(id = R.string.app_name), color = Color.White)
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Red
+            ),
+            navigationIcon = {
+                IconButton(onClick = { navController.navigate("myMainView") }) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Toggle Drawer",
+                        tint = Color.White
+                    )
+                }
+            })
     }
 }
